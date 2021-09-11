e0fb7bdbaa97ffb43165a4c046922dd5ffd6c65e
njmbb8 <nmcgr001@gmail.com>
2021-09-11 10:20:40 -0400

    completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,24 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const displayActors = actors.map((actor) => {
+    const moviesList = actor.movies.map((movie) => <li key = {movie}>{movie}</li>)
+
+    return(
+      <div key = {actor.name}>
+        <h2>{actor.name}</h2>
+        <ul>{moviesList}</ul>
+      </div>
+    )
+  })
+
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {displayActors}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,24 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const displayDirectors = directors.map((director) => {
+    const movieList = director.movies.map((movie) => <li key={movie}>{movie}</li>)
+
+    return(
+      <div key = {director.name}>
+        <h2>{director.name}</h2>
+        <ul>{movieList}</ul>
+      </div>
+    )
+  }) 
+
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {displayDirectors}
+    </div>
+  );
 }
 
 export default Directors;
